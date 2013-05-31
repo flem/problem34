caa384ea808dee9e0e5b1b1b93d1bf7984798bc2
flem789 <flemmingja@hotmail.com>
2013-05-31 16:23:49 +0200

    testkopi

---
+++ HelloWorld/src/CopyOfHelloHej.java
@@ -0,0 +1,12 @@
+
+public class CopyOfHelloHej {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello hej world, dette er test");
+	}
+
+}
